baf5b9e364136de88e65400db8097bae30986f4f
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-07-24 18:08:09 -0300

    Update q9.py

--- atividade-2/q9.py
+++ atividade-2/q9.py
@@ -8,9 +8,12 @@ dados = pd.read_csv("meu_arquivo.csv")
 
 # Selecionando uma coluna
 coluna2 = dados["Coluna 2"]
+coluna1 = dados["Coluna 1"]
 
 # Filtrando linhas com informações especificas
 dados_filtrados = dados[["Coluna 1", "Coluna 3"]][coluna2 != 'Dado 8,2']
+dados_filtrados2 = dados[["Coluna 2"]][coluna1 == 'Dado 6,1']
 
 # Exibindo primeiras linhas do Dataframe filtrado
 print(dados_filtrados.head())
+print(dados_filtrados2.head())
