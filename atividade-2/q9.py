5f1cea67194550c58f53eb5c8673044e91bd8f99
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-07-23 23:30:41 -0300

    Update q9.py

--- atividade-2/q9.py
+++ atividade-2/q9.py
@@ -1,3 +1,16 @@
 # Utilizando pandas, como selecionar uma coluna específica e filtrar linhas
 # em um “DataFrame” com base em uma condição?
 
+import pandas as pd
+
+# Lendo arquivo CSV
+dados = pd.read_csv("meu_arquivo.csv")
+
+# Selecionando uma coluna
+coluna2 = dados["Coluna 2"]
+
+# Filtrando linhas com informações especificas
+dados_filtrados = dados[["Coluna 1", "Coluna 3"]][coluna2 != 'Dado 8,2']
+
+# Exibindo primeiras linhas do Dataframe filtrado
+print(dados_filtrados.head())
