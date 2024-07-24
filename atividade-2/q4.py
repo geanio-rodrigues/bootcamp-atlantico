6d28d51cc83bd0b28da572926979970681ad335f
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-07-23 22:25:38 -0300

    Update q4.py

--- atividade-2/q4.py
+++ atividade-2/q4.py
@@ -1,2 +1,16 @@
 # Dada uma lista de números inteiros, escreva uma função para encontrar o
 # segundo maior valor na lista.
+
+def segundo_maior(lista):
+    """
+    Recebe uma lista de números inteiros
+    Retorna o segundo maior valor da lista
+    """
+    
+    lista.sort()  # Ordena a lista em ordem crescente
+    return lista[-2]  # Retorna o penúltimo valor da lista
+
+
+# Teste
+lista = [4, 1, 0, 8, 6, 12, 9]
+print(segundo_maior(lista))
