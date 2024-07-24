91a53d70d2589aab0424c3f21195837475f9c3c0
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-07-23 23:37:05 -0300

    Update q10.py

--- atividade-2/q10.py
+++ atividade-2/q10.py
@@ -1,3 +1,10 @@
 # Utilizando pandas, como lidar com valores ausentes (NaN) em um
 # DataFrame?
 
+import pandas as pd
+
+# Lendo arquivo CSV
+dados = pd.read_csv('meu_arquivo.csv')
+
+# Verificando a quantidade de valores ausentes por coluna
+print(dados.isnull().sum())
