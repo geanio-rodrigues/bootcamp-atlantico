ef009ee21eb9a8cbeddbe83c93fe1079295c5d39
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-07-23 23:13:55 -0300

    update q8.py

--- atividade-2/q8.py
+++ atividade-2/q8.py
@@ -1,3 +1,10 @@
 # Utilizando pandas, como realizar a leitura de um arquivo CSV em um
 # DataFrame e exibir as primeiras linhas?
 
+import pandas as pd
+
+# Lendo o aquivo CSV em um DataFrame
+dados = pd.read_csv('meu_arquivo.csv')
+
+# Exibindo as primeiras linhas
+print(dados.head())
