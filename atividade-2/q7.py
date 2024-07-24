6df70fbc7967dd289cd4fb821febddaa1b026063
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-07-23 23:10:13 -0300

    ...
    apenas apaguei e inseri uma letra novamente

--- atividade-2/q7.py
+++ atividade-2/q7.py
@@ -7,3 +7,12 @@
 # __, __ = plt.subplots()
 # ax.____(_, _)
 
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+x = np.linspace(-2 * np.pi, 2 * np.pi, 100)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
