343d73e26c9d20253a3cdc020e598db527a2e898
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-07-23 22:28:06 -0300

    Update q5.py

--- atividade-2/q5.py
+++ atividade-2/q5.py
@@ -2,3 +2,18 @@
 # nome e a idade de uma pessoa, e retorne a lista ordenada pelo nome das
 # pessoas em ordem alfabética.
 
+def ordena_nome(lista):
+    """
+    Recebe uma lista de nomes
+    Retorna uma nova lista contendo os nomes ordenados em ordem alfabética
+    """
+
+    lista_ordenada = lista.copy()  # Cria uma cópia da lista original
+    lista_ordenada.sort(key=lambda x: x[0])  # A lista de tulplas será ordenada pelo primeiro valor em cada tupla
+    return lista_ordenada
+
+
+# Teste
+lista = [("Bianca", 30), ("Alice", 25), ("Daniel", 28), ("Carlos", 20),
+         ("José", 31), ("Abraão", 17)]
+print(ordena_nome(lista))
