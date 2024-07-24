eb6f0bad982901db0a2ba7778a41f7a2a84bccae
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-07-23 22:21:11 -0300

    Update q2.py

--- atividade-2/q2.py
+++ atividade-2/q2.py
@@ -1,3 +1,24 @@
 # Escreva uma função que receba uma lista de números e retorne outra lista
 # com os números primos presentes.
 
+def primo(lista):
+    """
+    Recebe uma lista de número inteiros
+    Retorna uma nova lista contendo apenas os números primos.
+    """
+
+    lista_primo = []
+    for i in lista:
+        if i > 1:
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                lista_primo.append(i)
+
+    return lista_primo
+
+
+# Teste
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(primo(lista))
